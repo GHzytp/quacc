f0c7ec0157f28cf7d197544ea14a8dce66906023
GHzytp <GHzytp@users.noreply.github.com>
2026-06-23 16:30:30 +0000

    deploy: 6f88444e910e80395bbaa50e492a8af1bb05bd4b

--- gen_ref_pages.py
+++ gen_ref_pages.py
@@ -1,5 +1,7 @@
 """Generate the code reference pages and navigation."""
 
+from __future__ import annotations
+
 from pathlib import Path
 
 import mkdocs_gen_files
@@ -12,10 +14,10 @@ for path in sorted(Path("src").rglob("*.py")):
     full_doc_path = Path("reference", doc_path)
 
     parts = tuple(module_path.parts)
-    if parts[1] in ("presets", "_cli", "_version"):
-        continue
 
-    if parts[-1] in ("__init__", "__main__"):
+    ignore = ["_cli", "_version", "__init__", "__main__"]
+    skip = any(p in ignore for p in parts)
+    if skip:
         continue
 
     nav[parts] = doc_path.as_posix()
@@ -24,7 +26,7 @@ for path in sorted(Path("src").rglob("*.py")):
         ident = ".".join(parts)
         fd.write(f"::: {ident}")
 
-    mkdocs_gen_files.set_edit_path(full_doc_path, Path("../") / path)
+    mkdocs_gen_files.set_edit_path(full_doc_path, Path("..") / path)
 
-with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+with mkdocs_gen_files.open(Path("reference", "SUMMARY.md"), "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
